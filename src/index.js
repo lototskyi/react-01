30540e63e5f95b1378c5768c0b4572f9bc3c22dc
alex <o.lototskyi@gmail.com>
2022-02-02 12:33:56 +0200

    Prepare project, base JSX syntax (lesson 87)

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const text = 'Hello, World!';
+
+const elem = (
+    <div>
+        <h2 className="text">Text: {text}</h2>
+        <input type="text" />
+        <label htmlFor=''>Label</label>
+        <button tabIndex="0">Click</button>
+    </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const elem = React.createElement('h2', { className: 'greetings' }, 'Hello, World!');
+
+// const element = {
+//     type: 'h2',
+//     props: {
+//         className: 'greetings',
+//         children: 'Hello, World!'
+//     }
+// }
+
+ReactDOM.render(
+    elem,
+    document.getElementById('root')
+);
+    
