f7b7cf7f9c2574efb84fbf6ab470ac02126ad6ef
alex <o.lototskyi@gmail.com>
2022-02-16 11:48:22 +0200

    Styles in React. React-Bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Button} from './App';
+import BootstrapTest from './BootstrapTest';
 import styled from 'styled-components';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BigButton = styled(Button)`
     margin: 0 auto;
@@ -16,6 +19,7 @@ ReactDOM.render(
     <StrictMode>
         <App/>
         <BigButton as="a">Send report</BigButton>
+        <BootstrapTest/>
     </StrictMode>,
     document.getElementById('root')
 );
