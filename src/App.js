c448c1b48a94cdd0262096d4b5c1fdb0135311e2
alex <HuiF8chi>
2022-03-18 18:59:22 +0200

    Render-props

--- src/App.js
+++ src/App.js
@@ -139,9 +139,44 @@ const DynamicGreating = (props) => {
     );
 }
 
+const Message = (props) => {
+    return (
+        <h2>The counter is {props.counter}</h2>
+    )
+}
+
+class Counter extends Component {
+    state = {
+        counter: 0
+    }
+
+    changeCounter = () => {
+        this.setState(({counter}) => ({
+            counter: counter + 1
+        }));
+    }
+
+    render() {
+        return (
+            <>
+                <button 
+                    className={'btn btn-primary'}
+                    onClick={this.changeCounter}>
+                    Click me
+                </button>
+                {this.props.render(this.state.counter)}
+            </>
+        )
+    }
+}
+
 function App() {
   return (
     <Wrapper>
+        <Counter render={counter => (
+            <Message counter={counter} />
+        )} />
+
         <BootstrapTest
             left = {
                 <DynamicGreating color={'primary'}>
